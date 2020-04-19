b20595e69978c3b2b629098a8e2f78eb1d9bd940
Eli Nass <eliftw@gmail.com>
2020-04-19 23:20:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -32,5 +32,8 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   return_call = Nil
   valid_calls.each do |element|
-    planeteer_calls.include? element
+    if planeteer_calls.include? element
+      return_call = element
+      return return_call
+    end
 end
